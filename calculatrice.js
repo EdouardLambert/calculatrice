7fcbb260d78c6389c36b1ec52214bcde7d1e654e
Edouard Lambert <90192582+EdouardLambert@users.noreply.github.com>
2023-03-13 22:24:36 +0100

    Etape 2.2

---
+++ calculatrice.js
@@ -0,0 +1,13 @@
+console.log("Edouard is Here !");
+
+function log() {
+    console.log('Ce bouton est bien log');
+}
+
+function mathslog(chiffre) {
+    console.log(chiffre);
+}
+
+function actionlog(action) {
+    console.log(action);
+}
