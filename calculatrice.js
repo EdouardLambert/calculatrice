2c5284a7eb474f4f18a8be97337d8cbc7feddacd
EdouardLambert <edouard.lambert@student.junia.com>
2023-03-14 21:49:59 +0100

    petits commentaires

--- calculatrice.js
+++ calculatrice.js
@@ -1,37 +1,27 @@
 console.log("Edouard is Here !");
 
-function log() {
-    console.log('Ce bouton est bien log');
-}
+/* Fonctions log */
 
-function chiffrelog(chiffre) {
-    console.log(chiffre);
-}
+function log() { console.log('Ce bouton est bien log'); }
+function chiffrelog(chiffre) { console.log(chiffre); }
+function operationlog(operation) { console.log(operation); }
+function actionlog(action) { console.log(action); }
 
-function operationlog(operation) {
-    console.log(operation);
-}
 
-function actionlog(action) {
-    console.log(action);
-}
+
+/* Calculatrice */
 
 function ecriture(touche) {
-    if (document.getElementsByClassName("calcul")[0].value != " ") {
-        document.getElementsByClassName("calcul")[0].value += touche;
-    }
-    else {
-        document.getElementsByClassName("calcul")[0].value = touche;
-    }
+    document.getElementsByClassName("calcul")[0].value += touche; // On ajoute l'element tape a ce qui a ete ecrit precedemment  
 }
 
 function supprimer() {
-    valeur = document.getElementsByClassName("calcul")[0].value;
-    document.getElementsByClassName("calcul")[0].value = valeur.slice(0, valeur.length - 1);
+    valeur = document.getElementsByClassName("calcul")[0].value; // On recupere ce qui a ete tape precedemment
+    document.getElementsByClassName("calcul")[0].value = valeur.slice(0, valeur.length - 1); // On retire le dernier element
 }
 
 function reset() {
-    document.getElementsByClassName("calcul")[0].value = " ";
+    document.getElementsByClassName("calcul")[0].value = " "; // On vide ce qui a ete tape precedemment
 }
 
 function calculer() {
