6dac481801656ed1dbb8e24d124fb18739c49e63
EdouardLambert <edouard.lambert@student.junia.com>
2023-03-14 17:28:30 +0100

    Fin de la partie 1

--- calculatrice.js
+++ calculatrice.js
@@ -4,10 +4,41 @@ function log() {
     console.log('Ce bouton est bien log');
 }
 
-function mathslog(chiffre) {
+function chiffrelog(chiffre) {
     console.log(chiffre);
+}
+
+function operationlog(operation) {
+    console.log(operation);
 }
 
 function actionlog(action) {
     console.log(action);
+}
+
+function ecriture(touche) {
+    if (document.getElementsByClassName("calcul")[0].value != 0) {
+        document.getElementsByClassName("calcul")[0].value += touche;
+    }
+    else {
+        document.getElementsByClassName("calcul")[0].value = touche;
+    }
+}
+
+function supprimer() {
+    valeur = document.getElementsByClassName("calcul")[0].value;
+    document.getElementsByClassName("calcul")[0].value = valeur.slice(0, valeur.length - 1);
+}
+
+function reset() {
+    document.getElementsByClassName("calcul")[0].value = 0;
+}
+
+function calculer() {
+    calcul = document.getElementsByClassName("calcul")[0].value;
+    if (calcul == 0) return; // On ne calcule pas si l'utilisateur n'a rien saisie
+    calculDetail = calcul;
+    resultat = eval(calcul);
+    document.getElementsByClassName("previous")[0].innerHTML = calculDetail + " = " + resultat; // On affiche le calcul dans l'historique
+    document.getElementsByClassName("calcul")[0].value = resultat; // On reprend le resultat precedant pour le prochain calcul
 }
