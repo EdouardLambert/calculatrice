9320e9c90cc57c3fb7854764b9f8a5f03fb9a11f
EdouardLambert <edouard.lambert@student.junia.com>
2023-03-28 14:26:47 +0200

    j'ai oublié de mettre de mettre des let (je suis debile)

--- calculatrice.js
+++ calculatrice.js
@@ -22,7 +22,7 @@ class BaseCalculator {
     }
 
     supprimer() {
-        valeur = document.getElementsByClassName("calcul")[0].value; // On recupere ce qui a ete tape precedemment
+        let valeur = document.getElementsByClassName("calcul")[0].value; // On recupere ce qui a ete tape precedemment
         document.getElementsByClassName("calcul")[0].value = valeur.slice(0, valeur.length - 1); // On retire le dernier element
     }
 
@@ -31,10 +31,10 @@ class BaseCalculator {
     }
 
     calculer() {
-        calcul = document.getElementsByClassName("calcul")[0].value;
+        let calcul = document.getElementsByClassName("calcul")[0].value;
         if (calcul == 0) return; // On ne calcule pas si l'utilisateur n'a rien saisie
-        calculDetail = calcul;
-        resultat = eval(calcul);
+        let calculDetail = calcul;
+        let resultat = eval(calcul);
         document.getElementsByClassName("previous")[0].innerHTML = calculDetail + " = " + resultat; // On affiche le calcul dans l'historique
         document.getElementsByClassName("calcul")[0].value = resultat; // On reprend le resultat precedant pour le prochain calcul
     }
