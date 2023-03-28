9ee07f1aecd0214b4c70f5e4edfd17554f700d16
EdouardLambert <edouard.lambert@student.junia.com>
2023-03-28 14:12:19 +0200

    creation d'une classe (probleme sur les methodes suppr et egal)

--- calculatrice.js
+++ calculatrice.js
@@ -1,34 +1,44 @@
 console.log("Edouard is Here !");
 
-/* Fonctions log */
+class BaseCalculator {
 
-function log() { console.log('Ce bouton est bien log'); }
-function chiffrelog(chiffre) { console.log(chiffre); }
-function operationlog(operation) { console.log(operation); }
-function actionlog(action) { console.log(action); }
+    constructor() {
+        // Do some stuff
+    }
+
+    /* Fonctions log */
+
+    log() { console.log('Ce bouton est bien log'); }
+    chiffrelog(chiffre) { console.log(chiffre); }
+    operationlog(operation) { console.log(operation); }
+    actionlog(action) { console.log(action); }
 
 
 
-/* Calculatrice */
+    /* Calculatrice */
 
-function ecriture(touche) {
-    document.getElementsByClassName("calcul")[0].value += touche; // On ajoute l'element tape a ce qui a ete ecrit precedemment  
+    ecriture(touche) {
+        document.getElementsByClassName("calcul")[0].value += touche; // On ajoute l'element tape a ce qui a ete ecrit precedemment  
+    }
+
+    supprimer() {
+        valeur = document.getElementsByClassName("calcul")[0].value; // On recupere ce qui a ete tape precedemment
+        document.getElementsByClassName("calcul")[0].value = valeur.slice(0, valeur.length - 1); // On retire le dernier element
+    }
+
+    reset() {
+        document.getElementsByClassName("calcul")[0].value = " "; // On vide ce qui a ete tape precedemment
+    }
+
+    calculer() {
+        calcul = document.getElementsByClassName("calcul")[0].value;
+        if (calcul == 0) return; // On ne calcule pas si l'utilisateur n'a rien saisie
+        calculDetail = calcul;
+        resultat = eval(calcul);
+        document.getElementsByClassName("previous")[0].innerHTML = calculDetail + " = " + resultat; // On affiche le calcul dans l'historique
+        document.getElementsByClassName("calcul")[0].value = resultat; // On reprend le resultat precedant pour le prochain calcul
+    }
+
 }
 
-function supprimer() {
-    valeur = document.getElementsByClassName("calcul")[0].value; // On recupere ce qui a ete tape precedemment
-    document.getElementsByClassName("calcul")[0].value = valeur.slice(0, valeur.length - 1); // On retire le dernier element
-}
-
-function reset() {
-    document.getElementsByClassName("calcul")[0].value = " "; // On vide ce qui a ete tape precedemment
-}
-
-function calculer() {
-    calcul = document.getElementsByClassName("calcul")[0].value;
-    if (calcul == 0) return; // On ne calcule pas si l'utilisateur n'a rien saisie
-    calculDetail = calcul;
-    resultat = eval(calcul);
-    document.getElementsByClassName("previous")[0].innerHTML = calculDetail + " = " + resultat; // On affiche le calcul dans l'historique
-    document.getElementsByClassName("calcul")[0].value = resultat; // On reprend le resultat precedant pour le prochain calcul
-}
+let baseCalculator = new BaseCalculator();
